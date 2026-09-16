c3743ebb982e5e840bacda6bf5dd38b9e6e44d21
Daniel <danyrotupapi@gmail.com>
2026-09-15 23:31:50 -0500

    Ejemplo10

---
+++ #Variables Globales ejemplo 3.py
@@ -0,0 +1,13 @@
+#Variables Globales ejemplo 3
+inventario = [] # global
+def agregar(producto):
+   global inventario
+   inventario.append(producto)
+def mostrar():
+  for p in inventario:
+   print(f" - {p}")
+agregar("Laptop")
+agregar("Mouse")
+mostrar()
+# - Lapto 
+# - Mouse
