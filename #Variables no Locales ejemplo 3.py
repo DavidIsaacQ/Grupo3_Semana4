e294ab96fd405efb83750dd83139c5b422717e31
Daniel <danyrotupapi@gmail.com>
2026-09-15 23:39:07 -0500

    Ejemplo14

---
+++ #Variables no Locales ejemplo 3.py
@@ -0,0 +1,13 @@
+#Variables no Locales ejemplo 3
+def crear_interruptor():
+    estado = False
+
+    def cambiar():
+        nonlocal estado
+        estado = not estado
+        return "ON" if estado else "OFF"
+    return cambiar
+switch = crear_interruptor()
+print(switch()) # ON
+print(switch()) # OFF
+print(switch()) # ON
