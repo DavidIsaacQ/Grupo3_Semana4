988999f176abb1aa7d6c04e0005299b2d131135e
Daniel <danyrotupapi@gmail.com>
2026-09-15 23:38:24 -0500

    Ejemplo13

---
+++ #Variables no Locales ejemplo 2.py
@@ -0,0 +1,11 @@
+#Variables no Locales ejemplo 2
+def crear_acumulador():
+   total = 0
+   def acumular(valor):
+       nonlocal total
+       total += valor
+       return total
+   return acumular
+suma = crear_acumulador()
+print(suma(10)) # 10
+print(suma(5)) # 15
